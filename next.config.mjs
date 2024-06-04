84ebd54c797795ff39e58f64170941e55c1237b7
BradFernando <bfcorro@espe.edu.ec>
2024-06-04 15:22:00 -0500

    commit secundario

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/sign-in/[[...sign-in]]',
+        destination: '/app/sign-in/[[...sign-in]]/page',
+      },
+      {
+        source: '/sign-up/[[...sign-up]]',
+        destination: '/app/sign-up/[[...sign-up]]/page',
+      },
+      // Add more rewrites as needed
+    ];
+  },
+};
 
 export default nextConfig;
